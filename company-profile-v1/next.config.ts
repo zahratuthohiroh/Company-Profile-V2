aba3fe2b261709f564268622820b1f50e9fe0546
Chwyper <najwahikmatyar25@gmail.com>
2026-06-08 03:02:27 +0700

    chore: update next.config.ts for dynamic image hostname

--- company-profile-v1/next.config.ts
+++ company-profile-v1/next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
+        protocol: process.env.NEXT_PUBLIC_IMAGE_PROTOCOL === 'https' ? 'https' : 'http',
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME || '127.0.0.1',
+        port: process.env.NEXT_PUBLIC_IMAGE_PORT || '8000',
         pathname: '/storage/**',
       },
     ],
