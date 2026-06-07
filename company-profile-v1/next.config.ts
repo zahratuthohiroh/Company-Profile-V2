02f74bafa5028587ec11691b3bebe43fec740ee8
Chwyper <najwahikmatyar25@gmail.com>
2026-06-08 03:21:00 +0700

    fix: adjust image port logic for production

--- company-profile-v1/next.config.ts
+++ company-profile-v1/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         protocol: process.env.NEXT_PUBLIC_IMAGE_PROTOCOL === 'https' ? 'https' : 'http',
         hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME || '127.0.0.1',
-        port: process.env.NEXT_PUBLIC_IMAGE_PORT || '8000',
+        port: process.env.NEXT_PUBLIC_IMAGE_PORT !== undefined ? process.env.NEXT_PUBLIC_IMAGE_PORT : '8000',
         pathname: '/storage/**',
       },
     ],
